91b9b5001ba7d1e8f47c79fc87d5fd2b705b8dc2
Dustin Jones <info@domesticjones.com>
2019-07-17 14:12:47 -0600

    Pull data into footer, configure footer menus, style footer for desktop

--- footer.php
+++ footer.php
@@ -1,31 +1,55 @@
 			<?php if(have_rows('affiliates', 'options')): ?>
 				<footer id="affiliates">
-					<?php the_field('affillates_heading', 'options'); ?>
-					<?php while(have_rows('affiliates', 'options')): the_row(); ?>
-						<?php var_dump(get_sub_field('logo')); ?>
-						<?php var_dump(get_sub_field('link')); ?>
-					<?php endwhile; ?>
+					<h3><?php the_field('affiliates_heading', 'options'); ?></h3>
+					<ul class="footer-affiliates">
+						<?php while(have_rows('affiliates', 'options')): the_row(); $logo = get_sub_field('logo'); $link = get_sub_field('link'); ?>
+							<li>
+								<?php if($link) { echo '<a href="' . $link['url'] . '" target="' . $link['target'] . '" title="' . $link['title'] . '">'; } ?>
+									<img src="<?php echo $logo['sizes']['small']; ?>" alt="<?php echo $logo['alt']; ?>" />
+								<?php if($link) { echo '</a>'; } ?>
+							</li>
+						<?php endwhile; ?>
+					</ul>
 				</footer>
 			<?php endif; ?>
 			<footer id="footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
-				<div class="wrap">
-					<p class="copyright">&copy; Copyright <?php ex_brand('legal'); ?></p>
+				<section class="footer-column footer-column-logo">
+					<img src="<?php ex_logo('primary', 'light'); ?>" alt="Logo for <?php ex_brand(); ?>" class="footer-logo" />
+					<p class="copyright">&copy;<?php echo date('Y') . ' '; ex_brand('legal'); ?></p>
+				</section>
+				<section class="footer-column">
+					<h3 class="footer-heading">Contact</h3>
+					<p class="footer-address">
+						<?php ex_brand(); echo '<br /><span>'; ex_contact('address'); echo '</span>'; ?>
+					</p>
+				</section>
+				<section class="footer-column">
+					<h3 class="footer-heading">Services</h3>
 					<nav class="nav-footer" role="navigation">
-						<?php wp_nav_menu(array(
-							'container' => 'ul',                    // enter '' to remove nav container
-							'container_class' => 'footer-links cf',	// class of container (should you choose to use it)
-							'menu' => __('Footer', 'exonym'),	      // nav name
-							'menu_class' => 'nav footer-nav cf',    // adding custom nav class
-							'theme_location' => 'footer-menu',		  // where it's located in the theme
-							'before' => '',							            // before the menu
-							'after' => '',							            // after the menu
-							'link_before' => '',					          // before each link
-							'link_after' => '',						          // after each link
-							'depth' => 1,							              // limit the depth of the nav
-							'fallback_cb' => ''						          // fallback function
-						)); ?>
+						<?php
+							wp_nav_menu(array(
+								'container' => false,
+								'theme_location' => 'menu-services',
+								'depth' => 1,
+							));
+						?>
 					</nav>
-				</div>
+				</section>
+				<section class="footer-column">
+					<h3 class="footer-heading">Information</h3>
+					<nav class="nav-footer" role="navigation">
+						<?php
+							wp_nav_menu(array(
+								'container' => false,
+								'theme_location' => 'menu-info',
+								'depth' => 1,
+							));
+						?>
+					</nav>
+				</section>
+				<section class="footer-column">
+					<?php ex_social(); ?>
+				</section>
 			</footer>
 		</div>
 		<?php wp_footer(); ?>
