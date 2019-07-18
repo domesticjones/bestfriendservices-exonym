24335050a4bf0287a0a4c7dcbb545c8ca84580b8
Dustin Jones <info@domesticjones.com>
2019-07-18 03:59:30 -0600

    create global wrapper function, create all home page fields, input sample data

--- modules/index.php
+++ modules/index.php
@@ -1,1 +1,10 @@
-<?php //Silencio
+<?php
+
+function ex_wrap($pos, $class = '', $field = '') {
+  if($pos == 'start') {
+    $output = '<section id="' . $field . '" class="module module-' . $class . '">';
+  } elseif($pos == 'end') {
+    $output = '</section>';
+  }
+  echo $output;
+}
--- page-home.php
+++ page-home.php
@@ -2,7 +2,30 @@
   /* TEMPLATE NAME: Home */
   get_header();
   if(have_posts()): while(have_posts()): the_post();
-
+    ex_wrap('start', 'hero', 'hero');
+      echo 'hero image';
+    ex_wrap('end');
+    ex_wrap('start', 'cats', get_field('categories_module_id'));
+      echo 'categories';
+    ex_wrap('end');
+    ex_wrap('start', 'start', 'funnel');
+      echo 'funnel area';
+    ex_wrap('end');
+    ex_wrap('start', 'testimonials', get_field('testimonials_settings')['module_id']);
+      echo 'testimonials';
+    ex_wrap('end');
+    ex_wrap('start', 'topsellers', 'top-sellers');
+      echo 'top sellers';
+    ex_wrap('end');
+    ex_wrap('start', 'about', get_field('about_settings')['module_id']);
+      echo 'about';
+    ex_wrap('end');
+    ex_wrap('start', 'trending', 'trending');
+      echo 'trending';
+    ex_wrap('end');
+    ex_wrap('start', 'quote', get_field('quote_settings')['module_id']);
+      echo 'quote';
+    ex_wrap('end');
   endwhile; endif;
   get_footer();
 ?>
